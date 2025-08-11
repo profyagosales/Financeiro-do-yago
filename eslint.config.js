50506761f7eba48e3254da30c922dafdcca39fe5
profyagosales <prof.yago.red@gmail.com>
2025-08-11 19:23:25 -0300

    chore: enforce import order (#55)

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginImport from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
@@ -18,6 +19,9 @@ export default tseslint.config([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      import: pluginImport,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -25,6 +29,20 @@ export default tseslint.config([
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       'react-refresh/only-export-components': 'off',
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+        },
+      ],
     },
   },
 ], storybook.configs["flat/recommended"]);
