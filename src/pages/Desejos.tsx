fdd54ea5796787843189335c5f049903876a81a7
profyagosales <prof.yago.red@gmail.com>
2025-08-14 18:24:32 -0300

    feat: add wishes hero page

---
+++ src/pages/Desejos.tsx
@@ -0,0 +1,9 @@
+import { Logo } from '@/components/Logo';
+
+export default function Desejos() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-emerald-500 to-teal-600">
+      <Logo size="lg" />
+    </div>
+  );
+}
