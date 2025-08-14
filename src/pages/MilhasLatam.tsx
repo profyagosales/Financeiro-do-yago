a2ca28e84c78eef13603fdabac45561f7732e77d
profyagosales <prof.yago.red@gmail.com>
2025-08-14 10:48:46 -0300

    refactor: inline MilhasLivelo for LATAM (#143)

--- src/pages/MilhasLatam.tsx
+++ src/pages/MilhasLatam.tsx
@@ -1,8 +1,6 @@
 import MilhasLivelo from './MilhasLivelo';
 
-import { BRANDS } from '@/lib/brands';
-
 export default function MilhasLatam() {
   // Reuso da página principal, alterando apenas o programa.
-  return <MilhasLivelo program={BRANDS.latam} />;
+  return <MilhasLivelo program="latampass" />;
 }
