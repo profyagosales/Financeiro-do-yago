0720335754246a5e878413ff3f5dcc861f86cfc6
profyagosales <prof.yago.red@gmail.com>
2025-08-14 01:35:50 -0300

    use BRANDS constant for MilhasLatam

--- src/pages/MilhasLatam.tsx
+++ src/pages/MilhasLatam.tsx
@@ -1,7 +1,8 @@
 import MilhasLivelo from './MilhasLivelo';
 
+import { BRANDS } from '@/lib/brands';
+
 export default function MilhasLatam() {
   // Reuso da página principal, alterando apenas o programa.
-
-  return <MilhasLivelo program="latampass" />;
+  return <MilhasLivelo program={BRANDS.latam} />;
 }
