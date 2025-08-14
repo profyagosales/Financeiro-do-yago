ec058cb5c6d6aa13dc9cd1cfa2bbb2dfe44f539b
profyagosales <prof.yago.red@gmail.com>
2025-08-14 00:24:51 -0300

    feat: add program-specific mile headers (#86)

--- src/pages/MilhasLatam.tsx
+++ src/pages/MilhasLatam.tsx
@@ -1,7 +1,5 @@
 import MilhasLivelo from './MilhasLivelo';
+
 export default function MilhasLatam() {
-  // Reuso rápido: mesma página, trocando somente o título/badge
-  // Para personalizar (cores/regras), podemos separar depois.
-  // Como atalho, apenas exporto a versão reutilizada e você muda o título no PageHeader.
-  return <MilhasLivelo />;
+  return <MilhasLivelo program="latam" />;
 }
