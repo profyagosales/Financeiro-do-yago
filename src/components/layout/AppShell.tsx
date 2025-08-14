11cc5b95ecdf50f8413778754dbc65500bf5758b
profyagosales <prof.yago.red@gmail.com>
2025-08-14 20:27:26 -0300

    feat: add sidebar navigation

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren } from "react";
 
 import Topbar from "@/components/layout/Topbar";
+import Sidebar from "@/components/layout/Sidebar";
 import { cn } from "@/lib/utils";
 
 type AppShellProps = PropsWithChildren<{
@@ -9,9 +10,12 @@ type AppShellProps = PropsWithChildren<{
 
 export default function AppShell({ children, mainClassName }: AppShellProps) {
   return (
-    <>
-      <Topbar />
-      <main className={cn("p-6 pt-16 md:pt-20", mainClassName)}>{children}</main>
-    </>
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex flex-1 flex-col">
+        <Topbar />
+        <main className={cn("p-6 pt-16 md:pt-20", mainClassName)}>{children}</main>
+      </div>
+    </div>
   );
 }
