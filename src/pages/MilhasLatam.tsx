961d515dc2d67eca6e494c7c0f7c5103d215c5be
profyagosales <prof.yago.red@gmail.com>
2025-08-14 01:08:26 -0300

    fix(milhas): move exports to top-level and add branded header per program

--- src/pages/MilhasLatam.tsx
+++ src/pages/MilhasLatam.tsx
@@ -2,6 +2,6 @@ import MilhasLivelo from './MilhasLivelo';
 
 export default function MilhasLatam() {
   // Reuso da página principal, alterando apenas o programa.
-  
-  return <MilhasLivelo program="latam" />;
+
+  return <MilhasLivelo program="latampass" />;
 }
