0b9f46151aa96bdb6a16118a97c5b153e2b844d3
profyagosales <prof.yago.red@gmail.com>
2025-08-14 01:36:51 -0300

    fix(milhas): replace conflict markers with MilhasLivelo component

--- src/pages/MilhasLatam.tsx
+++ src/pages/MilhasLatam.tsx
@@ -1,7 +1,5 @@
-import MilhasLivelo from './MilhasLivelo';
+import MilhasLivelo from "./MilhasLivelo";
 
 export default function MilhasLatam() {
-  // Reuso da página principal, alterando apenas o programa.
-
   return <MilhasLivelo program="latampass" />;
 }
